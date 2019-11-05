e6e52cb94cc12c40fa43e392af8a22c1cad82f9c
Troy Hatchard <hatt2720@36stargirl.cs.uleth.ca>
2019-11-05 15:52:46 -0700

    Created Inventory.h

---
+++ include/Inventory.h
@@ -0,0 +1,16 @@
+/**
+ * CPSC 2720 Fall 2019
+ * Authors Austen Oviatt, Lambee Mangal, Troy Hatchard, Sarah Ren
+*/
+
+
+/**
+* Data structure to store the items contained within a room
+* or held by a player
+*/
+
+class Inventory {
+
+
+
+}
