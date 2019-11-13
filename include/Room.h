4c29f1b71677c517991da5db30ed7f87d2b4f2aa
Austen (Austen) Oviatt <ovia2720@36loki.cs.uleth.ca>
2019-11-13 16:07:01 -0700

    implementation file for Room

--- include/Room.h
+++ include/Room.h
@@ -10,46 +10,68 @@
 
 class Room
 {
-public:
-  Room(Coordinate c, Inventory inventory, bool isLocked, KeyID keys): {}
+  public:
+   /**
+    * Default room constructor
+    */
+  Room(string name, string description, Room exits): {}
 
-  /**
-  * Return the list of item currently in the room
-  */
-  Inventory getInventory();
-
-  /**
-  * Get the corresponding key ID that opens the room
-  */
-  KeyID getKeyID();
-
-  /**
-  * Return the locked status of the room
-  */
-  bool getLocked();
-
-  /**
-  * Change the locked status of the room
-  */
-  void setLocked();
-
-
-private:
-  /**
-  * The key that opens the room
-  */
-  KeyID Keys;
-
-  /**
-  * status of whether room is currently locked
-  */
-  bool isLocked;
-
-  /**
-  * Inventory of the room
-  */
-  Inventory inventory;
-
-
-
+   /**
+    * Default room destructor
+    */
+   virtual ~Room();
+   
+   /**
+    * status of whether room is currently locked
+    * @return locked state
+    */
+   bool locked;
+   
+   /**
+    * All the items in the room
+    */
+   Inventory inventory;
+   
+   /**
+    * The key required to unlock a room
+    */
+   KeyItem key;
+   
+   /**
+    * Sets the locked status of the room
+    * @return lock state of room
+    */
+   bool setLock();
+   
+   /**
+    * checks whether an exit exists
+    * @return does the room connect to desired room
+    */
+   bool hasExit();
+   
+   /**
+    * sends player to room through exit
+    * @return room on other side of exit
+    */
+   Room getExit();
+   
+   
+  private:
+   /**
+    * The key that opens the room
+    */
+   KeyID Keys;
+   
+   /**
+    * Inventory of the room
+    */
+   Inventory inventory;
+   
+   /**
+    * Characters in the room
+    */
+   Character characters;
+   
 };
+
+#endif // ROOM_H
