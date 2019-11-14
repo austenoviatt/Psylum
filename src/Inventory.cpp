3bab71d65052fa0a934a168abcedad6a267c740f
Austen (Austen) Oviatt <ovia2720@36mystique.cs.uleth.ca>
2019-11-14 16:34:06 -0700

    test for inventory

---
+++ src/Inventory.cpp
@@ -0,0 +1,94 @@
+/**
+ * @author Troy Hatchard
+ * @author Lambee Mangal
+ * @author Austen Oviatt
+ * @author Sarah Ren
+ * @date November 13, 2019
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include "Inventory.h"
+#include "Item.h"
+
+using namespace std;
+
+Inventory::Inventory() {
+
+}
+/**
+   * Checks to see if an item exists in the inventory
+   * @param the item to be searched for
+   * @return a bool of whether the item is in the inventory
+   */
+bool Inventory::hasItem(Item I) {
+	bool ItemFound = false;
+	// Iterate over all elements in Vector
+	 //store the name of the item
+	string ItemName = I.getName();
+
+	//iterate all items in the inventory
+	for (auto x : items)
+	{
+		//found the item
+		if (x.getName() == ItemName)
+		{
+			ItemFound = true;
+			break;
+		}
+	}
+	if (ItemFound)
+		return true;
+	else
+		//did not find the item
+		return false;
+}
+
+/**
+ * Adds an item to the inventory
+ * @param The item to be added
+ */
+void Inventory::addItem(Item I) {
+	items.push_back(I);
+}
+
+/**
+ * Removes an Item from the inventory
+ * @param the item to be removed
+ */
+bool Inventory::removeItem(Item I) {
+	bool ItemFound = false;
+	int indexTracker = 0;
+
+	string ItemName = I.getName();
+
+	for (auto x : items)
+	{
+		//found the item
+		if (x.getName() == ItemName)
+		{
+			//change the status and delete the item from the inventory vector
+			ItemFound = true;
+			items.erase(items.begin() + indexTracker);
+
+			return true;
+		}
+		indexTracker++;
+	}
+	//did not find the item, deletion failed and throw an error message
+	if (ItemFound == false) {
+    return false;
+	}
+
+}
+
+/**
+* @return The items in the inventory
+*/
+std::vector<Item> Inventory::getItems() {
+	return items;
+}
+
+
---
+++ test/testInventory.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include "Inventory.h"
+#include "Item.h"
+#include "gtest/gtest.h"
+
+
+
+	TEST(Inventory, addItem)
+	{
+
+			// TODO: Your test code here
+			Item I("Magic Mushroom", "Will have hallucination when consumed, may die from overdose", true);
+			Item I2("Lab Coat", "Normal looking lab coat, stolen from the lab", true);
+			Item I3("DECOY ITEM!!", "THIS ITEM DOESN'T EXIST, IT IS A DECOY", false);
+
+			Inventory v;
+			v.addItem(I);
+			v.addItem(I2);
+
+
+			EXPECT_EQ(I.getName(), v.getItems()[0].getName());
+			EXPECT_FALSE(v.removeItem(I3));
+			//EXPECT_THROW(v.removeItem(I3), item_not_found);
+		}
+
+
