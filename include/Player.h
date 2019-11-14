cfd14bedd4ce1077bab789ddb8db8a6726182b90
Lambah (Lambee) Mangal <manl2720@36antman.cs.uleth.ca>
2019-11-13 17:21:12 -0700

    Renamed includeSample.h to Player.h file and added.

---
+++ include/Player.h
@@ -0,0 +1,13 @@
+/**
+ * @author Troy Hatchard, Lambee Mangal, Austen Oviatt, Sarah Ren
+ * @date November 13, 2019
+*/
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+/**
+ * This class is for the player (the user) of the game
+*/
+
+#include "Inventory.h"
