9e7c68cd65481ee0367be0776e16df41ac87185a
Troy Hatchard <hatt2720@mailhost.cs.uleth.ca>
2019-11-16 13:00:46 -0700

    Committing just to pull

--- test/TestRoom.cpp
+++ test/TestRoom.cpp
@@ -9,9 +9,11 @@
 #include "Room.h"
 #include <vector>
 
+TEST(TestRoom, test)
+
 TEST(TestRoom, testLocked) {
   Inventory testInv;
-  Room("TestRoom", "This is the test room. ", false, )
+  Room("TestRoom", "This is the test room. ", false, testInv, 1, )
 }
 
 TEST(TestRoom, testExits) {
