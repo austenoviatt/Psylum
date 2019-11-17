b50ccc54d6ee057c80007f93036247bd478226a5
Troy Hatchard <hatt2720@mailhost.cs.uleth.ca>
2019-11-16 18:10:58 -0700

    Added development files dir and saveLoad tests
    This development directory is here so that we can write code for saving and loading even though the Game class isn't finished yet. This way we can run it independently from the rest of the project. So DONT add these files to your Psylum Codeblocks project!!

---
+++ developmentFiles/saveLoad/main.cpp
@@ -0,0 +1,35 @@
+//main for testing load
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+void loadGame(std::string filename);
+
+int main() {
+  loadGame("testSave.csv");
+
+  return 0;
+}
+
+std::string headers;
+
+void loadGame(std::string filename) {
+  std::ifstream saveFile;
+  saveFile.open(filename);
+
+  if(!saveFile.fail()){
+    getline(saveFile, headers);
+    std::cout << "These are the headers: " << headers << std::endl;
+    
+
+
+    std::string line;
+
+    while(!saveFile.eof()) {
+      getline(saveFile, line);
+      std::cout << line << std::endl;
+      std::cout << saveFile.eof();
+    }
+  } else std::cout << "Error: File doesn't exist!";
+}
