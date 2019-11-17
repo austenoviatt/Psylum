de47c263e86368a3a0591cd8517ce04993fb32fa
Troy Hatchard <hatt2720@mailhost.cs.uleth.ca>
2019-11-17 16:43:48 -0700

    Added blank LoadSave Class

---
+++ include/LoadSave.h
@@ -0,0 +1,24 @@
+/**
+ * CPSC 2720 Fall 2019
+ * @author Troy Hatchard
+ * copyright Castor - University of Lethbridge
+*/
+
+#ifndef LOADSAVE_H
+#define LOADSAVE_H
+
+
+class LoadSave
+{
+  public:
+    LoadSave();
+    virtual ~LoadSave();
+
+
+
+  protected:
+
+  private:
+};
+
+#endif // LOADSAVE_H
---
+++ src/LoadSave.cpp
@@ -0,0 +1,17 @@
+/**
+ * CPSC 2720 Fall 2019
+ * @author Troy Hatchard
+ * copyright Castor - University of Lethbridge
+*/
+
+#include "LoadSave.h"
+
+LoadSave::LoadSave()
+{
+  //ctor
+}
+
+LoadSave::~LoadSave()
+{
+  //dtor
+}
